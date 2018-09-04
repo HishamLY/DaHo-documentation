c47d609d9979540f56d8752702be27d9cb052811
HishamLY <hishamlazuardi@gmail.com>
2018-09-04 16:18:54 +0700

    Fix indentation style on gamecontroller class

---
+++ Framework/include/GameController.h
@@ -0,0 +1,38 @@
+#ifndef _GAMECONTROLLER_H_
+#define _GAMECONTROLLRE_H_
+
+#include <iostream>
+
+#include "RoboCupGameControlData.h"
+#include "minIni.h"
+
+#define DATA_SECTION   "Robot Data"
+#define INVALID_VALUE   -1024.0
+
+using namespace std;
+
+namespace Robot {
+  class GameController {
+    public :
+      int RobotNumber;
+      int TeamNumber;
+      uint8 m_target_goalcolour;
+      uint8 m_target_compass;
+      uint8 m_old_gamestate;
+      uint16 m_last_penalty;
+      
+      GameController();
+      RoboCupGameControlData Data;
+      int ByteConvert(int idxStart, char* ReceivePack, char* type);    //Convert Array of Byte to Short or Int
+
+      void parseData(char* ReceivePack);   //Parsing and Extract the Package receive
+      void showInterface();    //showing parsing package
+      void Process();
+      void LoadINISettings(minIni* ini);
+      void LoadINISettings(minIni* ini, const std::string &section);
+      void SaveINISettings(minIni* ini);
+      void SaveINISettings(minIni* ini, const std::string &section);
+  };
+};
+
+#endif
